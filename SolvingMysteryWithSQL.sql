84817ffd00a1a52389c491a4821d9bcd7fc05225
SashaSadovska <84362244+SashaSadovska@users.noreply.github.com>
2022-02-10 10:52:42 -0500

    Update SolvingMysteryWithSQL.sql

--- SolvingMysteryWithSQL.sql
+++ SolvingMysteryWithSQL.sql
@@ -10,21 +10,31 @@ Below is log of my SQL queries for completing the course task and solving the my
 
 SELECT description FROM crime_scene_reports WHERE month = 7 AND day = 28 AND street = 'Chamberlin Street';
 
+
+
 -- checking transcript of three witnesses who mentioned 'courthouse'
 
 SELECT name, transcript FROM interviews WHERE month = 7 AND day = 28 AND transcript like '%courthouse%';
+
+
 
 -- getting license plates numbers taped on security camera from 10:15 to 10:25 (witness #1)
 
 SELECT license_plate FROM courthouse_security_logs WHERE month = 7 AND day = 28 AND hour = 10 AND minute >= 15 AND minute <= 25;
 
+
+
 -- (account_number or amount) info from ATM on Fifer Street when suspect was withdrawing money on 2020/07/28 (witness #2)
 
 SELECT account_number FROM atm_transactions WHERE month = 7 AND day = 28 AND transaction_type = 'withdraw' AND atm_location = 'Fifer Street';
 
+
+
 -- all calls lasted less than a minute on 2020/07/28 (caller or receiver) (witness #3)
 
 SELECT caller FROM phone_calls WHERE month = 7 AND day = 28 AND duration < 60;
+
+
 
 -- flight ID from passangers list who's passport # (thief passport #  within all witnesses's info: caller phone # & bank account # & plate #)
 
@@ -33,6 +43,8 @@ SELECT flight_id FROM passengers WHERE passport_number IN
 WHERE phone_number IN (SELECT caller FROM phone_calls WHERE month = 7 AND day = 28 AND duration < 60)
 AND license_plate IN (SELECT license_plate FROM courthouse_security_logs WHERE month = 7 AND day = 28 AND hour = 10 AND minute >= 15 AND minute <= 25)
 AND account_number IN (SELECT account_number FROM atm_transactions WHERE month = 7 AND day = 28 AND transaction_type = 'withdraw' AND atm_location = 'Fifer Street'));
+
+
 
 -- The thief ESCAPED TO: destination airport city on earliest flight 07/29/2020 with thief passport # (from above)
 
@@ -44,6 +56,8 @@ WHERE phone_number IN (SELECT caller FROM phone_calls WHERE month = 7 AND day = 
 AND license_plate IN (SELECT license_plate FROM courthouse_security_logs WHERE month = 7 AND day = 28 AND hour = 10 AND minute >= 15 AND minute <= 25)
 AND account_number IN (SELECT account_number FROM atm_transactions WHERE month = 7 AND day = 28 AND transaction_type = 'withdraw' AND atm_location = 'Fifer Street')))
 AND month = 7 AND day = 29 ORDER BY hour LIMIT 1);
+
+
 
 -- The THIEF is: name from passenger list from the earliest flight (see above)
 
@@ -61,6 +75,8 @@ AND license_plate IN (SELECT license_plate FROM courthouse_security_logs WHERE m
 AND account_number IN (SELECT account_number FROM atm_transactions WHERE month = 7 AND day = 28 AND transaction_type = 'withdraw' AND atm_location = 'Fifer Street')))
 AND month = 7 AND day = 29 ORDER BY hour LIMIT 1));
 
+
+
 -- The ACCOMPLICE is: name from people list by phone number who was the call receiver from thief
 
 SELECT name FROM people WHERE phone_number =
@@ -77,3 +93,4 @@ WHERE phone_number IN (SELECT caller FROM phone_calls WHERE month = 7 AND day = 
 AND license_plate IN (SELECT license_plate FROM courthouse_security_logs WHERE month = 7 AND day = 28 AND hour = 10 AND minute >= 15 AND minute <= 25)
 AND account_number IN (SELECT account_number FROM atm_transactions WHERE month = 7 AND day = 28 AND transaction_type = 'withdraw' AND atm_location = 'Fifer Street')))
 AND month = 7 AND day = 29 ORDER BY hour LIMIT 1))));
+
