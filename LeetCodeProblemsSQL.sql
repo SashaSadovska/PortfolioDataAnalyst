bd1a1905b60d06671804089b573a402b4b23032d
SashaSadovska <84362244+SashaSadovska@users.noreply.github.com>
2022-02-10 10:59:50 -0500

    Update LeetCodeProblemsSQL.sql

--- LeetCodeProblemsSQL.sql
+++ LeetCodeProblemsSQL.sql
@@ -77,8 +77,7 @@ SELECT
 Write an SQL query to report the first name, last name, city, and state of each person in the "Person" table. 
 If the address of a personId is not present in the "Address" table, report null instead. */
 
-
-
 SELECT Person.firstName, Person.lastName, Address.city, Address.state
 FROM Person
 LEFT JOIN Address ON Person.personId = Address.personId
+
